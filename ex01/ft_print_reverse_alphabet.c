cd5d69130ed16401fc161dfc55beb0b70bc99ff6
Kenosi Mosiane <kmosiane@c5r1s8.wethinkcode.co.za>
2020-06-19 10:38:55 +0200

    Learning C

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmosiane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/18 10:54:48 by kmosiane          #+#    #+#             */
+/*   Updated: 2020/06/19 10:36:19 by kmosiane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+void ft_putchar (char c)
+{
+	write (1, &c, 1);
+}
+void ft_print_reverse_alphabet(void){
+
+	char letter;
+	letter = 'z';
+	while(letter >= 'a'){
+	
+		ft_putchar(letter);
+		letter--;
+	}
+	}
+	int main(void)
+	{
+		ft_print_reverse_alphabet();
+		return 0;
+	}
+
