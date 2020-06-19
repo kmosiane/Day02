27fe5087194a90e8109d1ab8130fa11f0ea6384f
Kenosi Mosiane <kmosiane@c5r1s8.wethinkcode.co.za>
2020-06-19 16:02:13 +0200

    My first C PROGRAM

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: kmosiane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 10:32:47 by kmosiane          #+#    #+#             */
-/*   Updated: 2020/06/19 09:46:59 by kmosiane         ###   ########.fr       */
+/*   Updated: 2020/06/19 16:01:14 by kmosiane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,6 @@ void ft_print_alphabet(void)
 		ft_putchar(letter);
 		letter++;
 	}
-}
-int main(void)
-{
-	ft_print_alphabet();
-	return 0;
 }
 
 
